763b14552912ed550a92842a940930f219ba91d9
Mikreselasie <abiymikreselasie21@gmail.com>
2026-02-11 23:01:59 +0300

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export const DEPARTMENTS = [
+  "CS",
+  "Mechanical Engineering",
+  "Biomedical Engineering",
+  "Math",
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+    value: dept,
+    label: dept
+}))
